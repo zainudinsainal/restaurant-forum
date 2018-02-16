cdc499db082ee1d1f6b9873b11ff4befe6c868fa
zainudinsainal <supernova37@gmail.com>
2018-02-16 11:23:38 +0800

    link category and restaurant model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+
+  has_many :restaurants, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
 
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
+  belongs_to :category, optional: true
 
 end
