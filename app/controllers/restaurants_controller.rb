ff189371eed20d8a729f188bd9f5305cdabcafc4
zainudinsainal <supernova37@gmail.com>
2018-02-18 06:57:04 +0800

    user can add comment on restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,6 +7,21 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = @restaurant.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
   end
+  
   resources :categories, only: :show
   root "restaurants#index"
 
