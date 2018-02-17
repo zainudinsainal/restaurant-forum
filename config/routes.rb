132a07692b1ade17c0f995346df9d8d4fdcf2134
zainudinsainal <supernova37@gmail.com>
2018-02-17 15:16:51 +0800

    setup category resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :restaurants, only: [:index, :show]
+  resources :categories, only: :show
   root "restaurants#index"
 
   namespace :admin do
