fffba360c4b2200b8da5944b2e7c8a97e102dc5a
zainudinsainal <supernova37@gmail.com>
2018-02-18 07:26:22 +0800

    user can read comments on restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,18 +10,6 @@ class RestaurantsController < ApplicationController
     @comment = Comment.new
   end
 
-  def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @comment = @restaurant.comments.build(comment_params)
-    @comment.user = current_user
-    @comment.save!
-    redirect_to restaurant_path(@restaurant)
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+  
 
 end
