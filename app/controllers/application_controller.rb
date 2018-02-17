b16311bf4e3d7c8ab5f64fbce48c75ad8bbf1de8
zainudinsainal <supernova37@gmail.com>
2018-02-17 08:44:59 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   private
 
   def authenticate_admin
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,4 @@
 class RestaurantsController < ApplicationController
-
-  before_action :authenticate_user!
   
   def index
   end
