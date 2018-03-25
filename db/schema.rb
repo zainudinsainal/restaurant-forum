fed39bb36ee101cfa14b9ec0af254ebe89ef6169
zainudinsainal <supernova37@gmail.com>
2018-03-25 08:04:57 +0800

    add favorites_count to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324152737) do
+ActiveRecord::Schema.define(version: 20180325000323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180324152737) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "category_id"
+    t.integer "favorites_count"
   end
 
   create_table "users", force: :cascade do |t|
