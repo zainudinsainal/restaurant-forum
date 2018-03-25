186c493f23240f68e22192127bfe05e8f4bf9f21
zainudinsainal <supernova37@gmail.com>
2018-03-25 11:27:43 +0800

    setup self-referential relationships within users table through followships

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,2 +1,6 @@
 class Followship < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :following, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
 end
