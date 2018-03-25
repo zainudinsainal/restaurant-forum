bd21ac0537f6e2607a620d3154b6f28d87d6e7a7
zainudinsainal <supernova37@gmail.com>
2018-03-25 12:29:01 +0800

    add unfollow button on user index page and destroy action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -12,4 +12,14 @@ class FollowshipsController < ApplicationController
     end
   end
 
+  def destroy
+    #Next, use the where method to pass the value for following_id in order to find a matching record.
+    #Note that you cannot use the find method here, because the find method can only be used with
+    #the primary key id and following_id is not the primary key.
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:alert] = "Followship destroyed"
+    redirect_back(fallback_location: root_path)
+  end
+
 end
